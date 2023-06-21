0d73993cd658bc47ecb441a83adf8a87c63897d4
yuko <ryoyu-fu@live.jp>
2023-06-21 10:57:53 +0900

    task new show create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,7 +56,7 @@ class TasksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
-      @task = task.find(params[:id])
+      @task = Task.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
