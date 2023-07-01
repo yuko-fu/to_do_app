f66de46cdaf566366d5af3ebe32c2fc856358aca
yuko <ryoyu-fu@live.jp>
2023-07-01 21:59:23 +0900

    sample date imput

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user == current_user
+      render "show"
+    else
+      redirect_to  new_session_path
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  # namespace :admin do
+    resources :users, only: [:new, :create, :show]
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
