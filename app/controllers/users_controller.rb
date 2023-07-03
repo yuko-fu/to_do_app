97979cac8ae1423fe0a6ec8a2cf2d07b37e932c7
yuko <ryoyu-fu@live.jp>
2023-07-03 10:54:09 +0900

    adminpage set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
   
+
+
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root 'tasks#index'
   resources :tasks
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  # namespace :admin do
-    resources :users, only: [:new, :create, :show]
-  # end
+  namespace :admin do
+    resources :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
