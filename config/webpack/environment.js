f81b5deff2a9158ade9da0c27eeb47a33e9775f8
yuko <ryoyu-fu@live.jp>
2023-07-08 22:04:00 +0900

    webpakker_environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery',
         Popper: 'popper.js'
