f85fd142f230cc2ebf9cd9fd1328c330739418a0
yuko <ryoyu-fu@live.jp>
2023-07-13 01:14:38 +0900

    rspec ok

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :label do
-    name { "label1" }
+    name { "hogehoge" }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -50,6 +50,8 @@ RSpec.describe 'タスク管理機能', type: :system do
 
   describe 'ラベル機能' do
     let!(:label) { FactoryBot.create(:label, name: "label3") }
+    let!(:user) { FactoryBot.create(:user) }
+    
     before do
       visit new_session_path
       fill_in 'session_email', with: 'dpro@mail.com'
@@ -67,7 +69,8 @@ RSpec.describe 'タスク管理機能', type: :system do
         select"高", from: "task_priority"
         
         click_on '登録する'
-        binding.pry
+        # binding.pry
+        
         sleep(1)
         click_on '一覧に戻る'
         expect(page).to have_content "label1"
@@ -87,24 +90,15 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        # 1. new_task_pathに遷移する（新規作成ページに遷移する）
-        # ここにnew_task_pathにvisitする処理を書く
         visit new_task_path
-        # 2. 新規登録内容を入力する
-        #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
-        # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-        # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      
         fill_in 'task_task_name', with: 'yarukoto'
         fill_in 'task_content', with: 'kontento'
         select"完了", from: "task_status"
         select"高", from: "task_priority"
-        # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-        # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
         click_on '登録する'
         click_on '一覧に戻る'
-        # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-        # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-        # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+        
         expect(page).to have_content 'yarukoto'
         expect(page).to have_content 'kontento'
         expect(page).to have_content '完了'
